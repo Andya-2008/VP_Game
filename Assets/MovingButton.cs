168eb47fe19aa8424f34386a5209a3664a70cb66
ANDREWLAPTOP\andre <andrewhyun@live.com>
2024-03-12 21:46:48 -0700

    webgl commit

--- Assets/MovingButton.cs
+++ Assets/MovingButton.cs
@@ -55,4 +55,40 @@ public class MovingButton : MonoBehaviour, IPointerEnterHandler, IPointerExitHan
             GameObject.Find("Andrew").GetComponent<Andrew_Controller>().jumped = false;
         }
     }
+}/*
+
+using UnityEngine;
+using System.Collections;
+using UnityEngine.EventSystems;
+
+public class MovingButton : MonoBehaviour, IPointerDownHandler, IPointerUpHandler
+{
+
+    public bool button1;
+    public bool button2;
+    public bool button3;
+
+    public void OnPointerDown(PointerEventData eventData)
+    {
+        if (button1)
+        {
+            GameObject.Find("Andrew").GetComponent<Andrew_Controller>().movingLeft = true;
+        }
+        if (button2)
+        {
+            GameObject.Find("Andrew").GetComponent<Andrew_Controller>().movingRight = true;
+        }
+        if (button3)
+        {
+            GameObject.Find("Andrew").GetComponent<Andrew_Controller>().jumped = true;
+        }
+    }
+
+    public void OnPointerUp(PointerEventData eventData)
+    {
+        GameObject.Find("Andrew").GetComponent<Andrew_Controller>().movingLeft = false;
+        GameObject.Find("Andrew").GetComponent<Andrew_Controller>().movingRight = false;
+        GameObject.Find("Andrew").GetComponent<Andrew_Controller>().jumped = false;
+    }
 }
+*/
